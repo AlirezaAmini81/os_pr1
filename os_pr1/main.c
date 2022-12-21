3e9baea98738a0a9d0aaac8cdd446b91bd96dc74
tahmine <thminh5448@gmail.com>
2022-12-21 22:42:49 +0330

    add command history

--- os_pr1/main.c
+++ os_pr1/main.c
@@ -3,76 +3,151 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <unistd.h>
-#define RL_BUFSIZE 1024
-#define TOK_BUFSIZE 64
-#define TOK_DELIM " \t\r\n\a"
+#include <sys/types.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-void shell_program();
+#define clear() printf("\033[H\033[J");
+#define LSH_RL_BUFSIZE 1024
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
 
-char *read_line();
-
-
-char **split(char *line);
-
-int execute(char **args);
-
+//biult-in functions
 int lsh_cd(char **args);
-
+int lsh_help(char **args);
 int lsh_exit(char **args);
 
-/*
-  List of builtin commands, followed by their corresponding functions.
- */
+// List of builtin commands
 char *builtin_str[] = {
         "cd",
+        "help",
         "exit"
 };
 
+// List of builtin functions
 int (*builtin_func[]) (char **) = {
         &lsh_cd,
-
+        &lsh_help,
         &lsh_exit
 };
 
-int launch(char **args);
-
-int num_builtins() {
+int lsh_num_builtins() {
     return sizeof(builtin_str) / sizeof(char *);
 }
 
-//main
+/*
+  begin builtin function implementations
+*/
 
-int main(int argc, char **argv)
-{
-// Load config files, if any.
-
-    // Run command loop.
-    shell_program();
-
-    // Perform any shutdown/cleanup.
-
-    return EXIT_SUCCESS;
+int lsh_cd(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("lsh");
+        }
+    }
+    return 1;
 }
 
-void shell_program(void)
-{
-    char *line;
-    char **args;
+int lsh_help(char **args) {
+    int i;
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for (i = 0; i < lsh_num_builtins(); i++) {
+        printf("  %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
+
+int lsh_exit(char **args) {
+    return 0;
+}
+
+/*
+  end builtin function implementations
+*/
+
+// lunch system command
+int sys_launch(char **args) {
+    pid_t pid, wpid;
     int status;
 
-    do {
-        printf("> ");
-        line = read_line();
-        args = split(line);
-        status = execute(args);
+    pid = fork();
 
-        free(line);
-        free(args);
-    } while (status);
+    if (pid < 0){
+        // error forking
+        perror("\nFailed forking child..");
+    } else if (pid == 0) {
+        // Child process
+        if (execvp(args[0], args) == -1) {
+            perror("Could not execute command... ");
+        }
+        exit(EXIT_FAILURE);
+    } else {
+        // Parent process
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
 }
-char *read_line(void){
 
-    int bufsize = RL_BUFSIZE;
+// execute command
+int cmnd_execute(char **args) {
+    int i;
+
+    if (args[0] == NULL) {
+        // an empty command was entered
+        return 1;
+    }
+
+    for (i = 0; i < lsh_num_builtins(); i++) {
+        if (strcmp(args[0], builtin_str[i]) == 0) {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    return sys_launch(args);
+}
+
+char **split_line(char *line){
+    int bufsize = LSH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, LSH_TOK_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += LSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens) {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, LSH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+// read input line
+char *read_line(void){
+    int bufsize = LSH_RL_BUFSIZE;
     int position = 0;
     char *buffer = malloc(sizeof(char) * bufsize);
     int c;
@@ -97,7 +172,7 @@ char *read_line(void){
 
         // If we have exceeded the buffer, reallocate.
         if (position >= bufsize) {
-            bufsize += RL_BUFSIZE;
+            bufsize += LSH_RL_BUFSIZE;
             buffer = realloc(buffer, bufsize);
             if (!buffer) {
                 fprintf(stderr, "lsh: allocation error\n");
@@ -107,105 +182,51 @@ char *read_line(void){
     }
 }
 
-int execute(char **args) {
-
-    int i;
-
-    if (args[0] == NULL) {
-        // An empty command was entered.
-        return 1;
-    }
-
-    for (i = 0; i < num_builtins(); i++) {
-        if (strcmp(args[0], builtin_str[i]) == 0) {
-            return (*builtin_func[i])(args);
-        }
-    }
-
-    return launch(args);
+// print current directory
+void printDir() {
+	char cwd[1024];
+	getcwd(cwd, sizeof(cwd));
+	printf("\n~%s", cwd);
 }
 
-int launch(char **args)
-{
-    pid_t pid, wpid;
+// command loop
+void shell_program(void) {
+    char *line;
+    char **args;
     int status;
 
-    pid = fork();
-    if (pid == 0) {
-        // Child process
-        if (execvp(args[0], args) == -1) {
-            perror("lsh");
-        }
-        exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        // Error forking
-        perror("lsh");
-    } else {
-        // Parent process
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+    do {
+        printDir();
+        line = readline("\n> ");
+        if (line && *line)
+            add_history(line);
+        args = split_line(line);
+        status = cmnd_execute(args);
 
-    return 1;
+        //free(line);
+        free(args);
+    } while (status);
 }
 
-
-char **split(char *line){
-
-    int bufsize = TOK_BUFSIZE, position = 0;
-    char **tokens = malloc(bufsize * sizeof(char*));
-    char *token;
-
-    if (!tokens) {
-        fprintf(stderr, "lsh: allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    token = strtok(line, TOK_DELIM);
-    while (token != NULL) {
-        tokens[position] = token;
-        position++;
-
-        if (position >= bufsize) {
-            bufsize += TOK_BUFSIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if (!tokens) {
-                fprintf(stderr, "lsh: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        token = strtok(NULL, TOK_DELIM);
-    }
-    tokens[position] = NULL;
-    return tokens;
+// greeting shell during startup
+void init_shell() {
+	clear();
+	printf("\n\t\t\t****WELCOME TO MY SHELL****\n");
+	sleep(1);
+	clear();
 }
 
-
-
-
+int main(int argc, char **argv) {
+    // Load config files, if any.
+    init_shell();
+    // Run command loop.
+    shell_program();
+    // Perform any shutdown/cleanup.
+    return EXIT_SUCCESS;
+}
 
 
 /*
-  Builtin function implementations.
+gcc main.c -lreadline
+./a.out 
 */
-int lsh_cd(char **args)
-{
-    if (args[1] == NULL) {
-        fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-    } else {
-        if (chdir(args[1]) != 0) {
-            perror("lsh");
-        }
-    }
-    return 1;
-}
-
-
-int lsh_exit(char **args)
-{
-    return 0;
-}
-
-
